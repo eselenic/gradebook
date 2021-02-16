64be42369c47c01a19156d2bb122425348a2c152
eselenic <selenicaelona@gmail.com>
2021-02-15 19:23:38 -0600

    update name for readability

---
+++ server/models/classgrade.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let ClassGrade = new Schema({
+    school_id: {
+        type: Number
+    },
+    class_id: {
+        type: Schema.Types.ObjectId
+    },
+    assignments: {
+        type: [Schema.Types.ObjectId]
+    },
+    overall_grade: {
+        type: Number
+    }
+});
+
+module.exports = mongoose.model('ClassGrade', ClassGrade);
